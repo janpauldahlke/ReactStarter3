d6d4f18d8d2400e54fca31f9bd1f91426aa5d81b
janpauldahlke <janpauldahlke@gmail.com>
2017-06-18 11:54:01 +0200

    basics: Testing App

--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -1,14 +1,24 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect }  from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+//group similar tests -> App
+describe('Testing App', () =>{
 
   beforeEach(() => {
-    component = renderComponent(App);
+    //console.log('run App test beforeEach');
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
+  //use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
+
+    //helper to render the App Component
+    const component = renderComponent(App);
+    /*use 'expect' to make an assertion about target
+    //  syntax is
+    //  expect(component).to.have.class('selector')
+    //  keyword(thingToTest).Assertion.matcher(selector)
+    */
+    expect(component).to.contain('React simple starter');
   });
+
 });
