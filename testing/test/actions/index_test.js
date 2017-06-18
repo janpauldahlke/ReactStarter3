c20b690c52e9a76aae4e8e03e8c7e88526189c2c
janpauldahlke <janpauldahlke@gmail.com>
2017-06-18 16:36:57 +0200

    improved action_tests

--- testing/test/actions/index_test.js
+++ testing/test/actions/index_test.js
@@ -1,18 +1,25 @@
 //goofy filename to reflect prject structures
 
 import { expect } from '../test_helper';
-//import Actions from '..'
+import { SAVE_COMMENT} from '../../src/actions/types';
+// webpack points to index so: ../../src/actions/index
+import { saveComment} from '../../src/actions/index'
 
 describe('Testing actions', () => {
 
   describe('Action: save a comment', () => {
 
+    let action;
+
     it('has the correct Type', () => {
-      
+      action = saveComment();
+      //console.log(action);
+      expect(action.type).to.equal(SAVE_COMMENT);
     });
 
     it('has the correct payload', () => {
-
+      action = saveComment('new saved comment');
+      expect(action.payload).to.equal('new saved comment');
     });
 
   });
