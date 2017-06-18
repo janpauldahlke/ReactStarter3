da1d606b491b41983c2e4f9ec75d4b1ebbec501d
janpauldahlke <janpauldahlke@gmail.com>
2017-06-18 16:57:09 +0200

    prepare ComemntsReducer and CommentsReducer_test

---
+++ testing/test/reducers/comments_test.js
@@ -0,0 +1,13 @@
+import { expect } from '../test_helper';
+import CommentReducer from '../../src/reducers/comments';
+import { SAVE_COMMENT } from '../../src/actions/types';
+
+describe('Tests the CommentsReducer', () => {
+  it('handles action with unknow type', () => {
+
+  });
+
+  it('handles action of type SAVE_COMMENT', () => {
+    
+  });
+});
