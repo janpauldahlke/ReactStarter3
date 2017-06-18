6411c341ab451dcc3ab998a2e37f09db6eebaf63
janpauldahlke <janpauldahlke@gmail.com>
2017-06-18 12:35:13 +0200

    added CommentBox to App, wrote Test to check existence of .comment-box in App

--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -4,20 +4,25 @@ import App from '../../src/components/app';
 //group similar tests -> App
 describe('Testing App', () =>{
 
-  let component
+  let component;
+
   beforeEach(() => {
     component = renderComponent(App);
   });
 
   //use 'it' to test a single attribute of a target
   it('shows the correct text', () => {
-
     /*use 'expect' to make an assertion about target
     //  syntax is
     //  expect(component).to.have.class('selector')
     //  keyword(thingToTest).Assertion.matcher(selector)
     */
     expect(component).to.contain('React simple starter');
+  });
+
+  it('CommentBox is displayed as child in App', () => {
+    //find by Class (-> test against valid HTML)
+    expect(component.find('.comment-box')).to.exist;
   });
 
 });
