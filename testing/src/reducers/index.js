84aca5db0691f9666f9da34ba22e9a2566a4fe06
janpauldahlke <janpauldahlke@gmail.com>
2017-06-18 17:12:31 +0200

    add CommentsReducer to combineReducer

--- testing/src/reducers/index.js
+++ testing/src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
+import CommentsReducer from './comments';
 
 const rootReducer = combineReducers({
   //fighting errors on missing state
-  comments : (state = []) => state
-
+  comments : CommentsReducer
   //original
   //state: (state = {}) => state
 });
