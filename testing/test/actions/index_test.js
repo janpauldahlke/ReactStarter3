4ae5f77fe5309413f26ecbbc6bb7dfc8eece1123
janpauldahlke <janpauldahlke@gmail.com>
2017-06-18 16:26:55 +0200

    write ActionCreator tests

---
+++ testing/test/actions/index_test.js
@@ -0,0 +1,19 @@
+//goofy filename to reflect prject structures
+
+import { expect } from '../test_helper';
+//import Actions from '..'
+
+describe('Testing actions', () => {
+
+  describe('Action: save a comment', () => {
+
+    it('has the correct Type', () => {
+      
+    });
+
+    it('has the correct payload', () => {
+
+    });
+
+  });
+});
