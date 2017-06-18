d1e2b30a0b4a5ba2ea05ff355ef4d4c1401cfe85
janpauldahlke <janpauldahlke@gmail.com>
2017-06-18 17:04:56 +0200

    updated CommentsReducer Test

--- testing/test/reducers/comments_test.js
+++ testing/test/reducers/comments_test.js
@@ -3,11 +3,16 @@ import CommentReducer from '../../src/reducers/comments';
 import { SAVE_COMMENT } from '../../src/actions/types';
 
 describe('Tests the CommentsReducer', () => {
-  it('handles action with unknow type', () => {
 
+  it('handles action with unknow type', () => {
+    //give empty reducer
+    //expect(CommentReducer()).to.be.instanceof(Array);
+    //eql is a deep structure eql
+    expect(CommentReducer()).to.be.eql([]);
   });
 
   it('handles action of type SAVE_COMMENT', () => {
-    
+    const action = { type: SAVE_COMMENT, payload : 'new test comment'};
+    expect(CommentReducer([], action)).to.eql(['new test comment']);
   });
 });
