6bc0245ccb897a76e87439c81396b6df3b5db678
janpauldahlke <janpauldahlke@gmail.com>
2017-06-18 12:24:55 +0200

    prepare beforeEach() to improve codequality

--- testing/test/components/comment_box_test.js
+++ testing/test/components/comment_box_test.js
@@ -3,21 +3,29 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('Testing CommentBox', () => {
 
+  //give component let
+  let component;
+
+  beforeEach((done) => {
+    //render Element on component, call done();
+    component = renderComponent(CommentBox);
+    done();
+  });
+
+
+
   // matchers reference
   // https://github.com/chaijs/chai-jquery
   it('has a the correct className', () => {
-    const component = renderComponent(CommentBox);
     expect(component).to.have.class('comment-box');
   });
 
   it('Component has TextArea', () => {
   //no params() on exist !!
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('Component has Button', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
   });
 });
