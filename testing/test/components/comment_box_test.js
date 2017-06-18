713b1c84ae578b8f8c9583915ab87169a0d11a05
janpauldahlke <janpauldahlke@gmail.com>
2017-06-18 12:13:26 +0200

    create commentbox with minimum specc to pass test

--- testing/test/components/comment_box_test.js
+++ testing/test/components/comment_box_test.js
@@ -3,17 +3,17 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('Testing CommentBox', () => {
 
-  const component = renderComponent(CommentBox);
-
   // matchers reference
   // https://github.com/chaijs/chai-jquery
 
   it('Component has TextArea', () => {
   //no params() on exist !!
+    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('Component has Button', () => {
+    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
   });
 });
