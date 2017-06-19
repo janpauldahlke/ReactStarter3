f82a58e78bc5c787531111018bdd2c0c8feeb919
janpauldahlke <janpauldahlke@gmail.com>
2017-06-19 11:19:19 +0200

    TODO rewrite testhelper

---
+++ testing/test/test_helper.js
@@ -0,0 +1,6 @@
+/*  TODO rewrite test_helper
+  1. set up testing env to run like a browser
+  2. build 'renderComponent' helper, that renders a react class
+  3. build helper for simulating events
+  4. set up chaiJquery
+*/
