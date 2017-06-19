57072365a3aa962975e4255bd50ce40035d949a4
janpauldahlke <janpauldahlke@gmail.com>
2017-06-19 11:33:36 +0200

    prepare fake DOM with jsdom and connect jquery to this

--- testing/test/test_helper.js
+++ testing/test/test_helper.js
@@ -4,3 +4,15 @@
   3. build helper for simulating events
   4. set up chaiJquery
 */
+
+
+import jsdom from 'jsdom';
+import _$ from 'jquery';
+
+
+//create fake html and assign it to global (scope) document
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+//https://developer.mozilla.org/en-US/docs/Web/API/Document/defaultView#Syntax
+global.window = global.document.defaultView;
+//wire up jquery and jsdom
+const $ = _$(global.window);
