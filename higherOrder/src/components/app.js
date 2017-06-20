9057bdb666ad1ae604df7b8fb208a8c9206cd3af
janpauldahlke <janpauldahlke@gmail.com>
2017-06-20 09:35:37 +0200

    update README with goal to achieve

--- higherOrder/src/components/app.js
+++ higherOrder/src/components/app.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div>React Boilerplate</div>
     );
   }
 }
-
-function mapStateToProps(state) {
-  return { posts: state.props };
-}
-export default connect(mapStateToProps)(App);
