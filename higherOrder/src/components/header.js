ad81be7642b3763d0e56115cbc5ce0c49d3042eb
janpauldahlke <janpauldahlke@gmail.com>
2017-06-22 11:07:38 +0200

    wired action to concrete button using connect()() from react redux

--- higherOrder/src/components/header.js
+++ higherOrder/src/components/header.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Header extends Component {
 
   authButton(){
-    return <button className="btn">Sign In</button>;
+
+    if (this.props.authenticated) {
+      // refers to function name of exported func in index.js
+        return <button onClick={() => this.props.authenticate(false)} className="btn">Sign out</button>
+    }
+
+    return <button onClick={() => this.props.authenticate(true)} className="btn">Sign In</button>;
   }
 
   render() {
@@ -28,4 +36,11 @@ class Header extends Component {
 }
 
 
-export default Header;
+function mapStateToProps(state){
+  //return as * from reducer key field
+  return { authenticated : state.authenticated}
+}
+
+//through this we can refer to this.props.authenticated props are bind to state
+//also pass actions as nd arg in connect (1)
+export default connect(mapStateToProps, actions)(Header);
