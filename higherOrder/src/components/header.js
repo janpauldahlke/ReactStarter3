42c53ee21912e1ab36826bfd4b14d7f00983c625
janpauldahlke <janpauldahlke@gmail.com>
2017-06-22 09:37:12 +0200

    create Header component

---
+++ higherOrder/src/components/header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Header extends Component {
+
+  authButton(){
+
+      return(
+        <button>Sign In</button>
+      );
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-ligth">
+        <ul className="navbar navbar-nav">
+          <li className="nav-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/resssources">Ressources</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/"></Link>
+              { this.authButton() }
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+
+export default Header;
