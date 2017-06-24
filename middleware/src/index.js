2dc8406e1ac2c0995723de02a819b8dfe7e39d89
janpauldahlke <janpauldahlke@gmail.com>
2017-06-24 12:12:50 +0200

    boilerplate init

--- middleware/src/index.js
+++ middleware/src/index.js
@@ -2,23 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, Route, browserHistory } from 'react-router';
 
-//import HOC and use it as Wrapped Component in Route path component
-//import requireAUTH from './components/require_auth';
 import App from './components/app';
-import Ressources from './components/ressources';
 import reducers from './reducers';
-
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={ browserHistory }>
-      <Route path ="/" component={App} >
-        <Route path="/Ressources" component={ Ressources } ></Route>
-      </Route>
-    </Router>
+    <App />
   </Provider>
   , document.querySelector('#root'));
