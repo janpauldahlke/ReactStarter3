d89927d6c4080c1fd9f9c59569c8c96717a1348f
janpauldahlke <janpauldahlke@gmail.com>
2017-06-24 08:47:04 +0200

    make use of HOC in index.js

--- higherOrder/src/components/require_auth.js
+++ higherOrder/src/components/require_auth.js
@@ -7,7 +7,7 @@ import React, { Component } from 'react';
     1.  do not export a class, but wrap it
     2. in the function create a class
     3. render it in the return
-    4. spread this.props on component
+    4. spread ...this.props on component to make props accsible also to the inner component
     5.  return the inner Class within the function
 //
 
@@ -20,6 +20,16 @@ export default function (ComposedComponent) {
   return Authentication;
 }
 */
+
+//in other file .. not this file
+// we want to use it like this
+
+//import Authentication;
+//component that is to be wr^apped
+//import Ressources
+//const ComposedComponent = Authentication(Ressources);
+//in some render method ...
+//<ComposedComponent />
 
 
 export default function (ComposedComponent) {
--- higherOrder/src/index.js
+++ higherOrder/src/index.js
@@ -4,9 +4,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
 
+//import HOC and use it as Wrapped Component in Route path component
+import requireAUTH from './components/require_auth';
 import App from './components/app';
 import Ressources from './components/ressources';
 import reducers from './reducers';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -14,7 +17,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={ browserHistory }>
       <Route path ="/" component={App} >
-        <Route path="/Ressources" component={Ressources} ></Route>
+        <Route path="/Ressources" component={requireAUTH(Ressources)} ></Route>
       </Route>
     </Router>
   </Provider>
