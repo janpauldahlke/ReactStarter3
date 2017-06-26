d3072b530e8c2a502d29a3c531d8239c805b998e
janpauldahlke <janpauldahlke@gmail.com>
2017-06-26 08:12:18 +0200

    resolved promise in middleware and shown API endpoint /usersList

--- middleware/src/middleware/async.js
+++ middleware/src/middleware/async.js
@@ -2,19 +2,31 @@
 //  1. call dispatch
 //  2. call next
 //  3. call action
+//  next calls the next middleware, if there is none pipe to reducer
 
 
 export default function({ dispatch }){
-  /*return next => action => {
-    console.log(action);
-    next(action);
-  };*/
+  return next => action => {
+    //action doesnt have a payload or funny way to ask for promise.then prop
+    if( !action.payload || !action.payload.then ){
+      return next(action);
+    }
+    //resolve promise
+    action.payload
+      .then(function(response) {
+        //create a new action with the old type
+        //but replace the promise with the response data
+        const newAction =  {...action, payload:response};
+        //return val via dispatch
+        dispatch(newAction);
+      });
+  }
 
   //vanilla ex5
-  return function( next ) {
+  /*return function( next ) {
     return function( action ){
       console.log( action );
       next(action);
     }
-  }
+  }*/
 }
